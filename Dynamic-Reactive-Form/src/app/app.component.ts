68c265e85adbb5c260cd8efb8177bf2cb1a4e5ee
mtr-prabhat-sharma <142592099+mtr-prabhat-sharma@users.noreply.github.com>
2025-07-09 23:15:40 +0530

    Dynamic reactive forms

--- Dynamic-Reactive-Form/src/app/app.component.ts
+++ Dynamic-Reactive-Form/src/app/app.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor() {}
   title = 'Dynamic-Reactive-Form';
+  dynamicForm!: FormGroup;
+  formConfig = [
+    {
+      type: 'text',
+      label: 'Full Name',
+      name: 'fullName',
+      validators: ['required'],
+    },
+    {
+      type: 'email',
+      label: 'Email Address',
+      name: 'email',
+      validators: ['required', 'email'],
+    },
+    {
+      type: 'select',
+      label: 'Country',
+      name: 'country',
+      options: ['India', 'USA', 'UK'],
+      validators: ['required'],
+    },
+  ];
+
+  ngOnInit(): void {
+    const group: any = {};
+   
+    this.formConfig.forEach((item) => {
+       const validators = [];
+      if (item.validators.includes('required')) {
+        validators.push(Validators.required);
+      }
+      if (item.validators.includes('email')) {
+        validators.push(Validators.email);
+      }
+      group[item.name] = new FormControl('', validators)
+    });
+    this.dynamicForm = new FormGroup(group);
+  }
+
+  onSubmit() {
+    console.log('Form Values', this.dynamicForm.value);
+  }
 }
